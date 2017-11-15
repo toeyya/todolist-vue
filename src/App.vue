3d65c53c937397d55b6c39d54b97d939e89d565b
araya tansinchai <toey@odd-e.co.th>
2017-11-15 15:11:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+ 
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <todo-list :todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */ 
+import TodoList from './components/TodoList'
+export default {
+  name: 'app',
+  components: {
+    // Add a reference to the TodoList component in the components property
+    TodoList,
+  },
+  data() {
+    return {
+      todos : [{
+          title: 'Todo A',
+          project: 'Project A',
+          done: false
+      },
+      {
+          title: 'Todo B',
+          project: 'Project B',
+          done: true
+      },
+      {
+          title: 'Todo C',
+          project: 'Project C',
+          done: false
+      },
+      {
+          title: 'Todo D',
+          project: 'Project D',
+          done: true
+      }]
+    };
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <!-- <div class="ui centered card" v-for="todo in todos">
+            <div class="content">
+                <div class="header">{{todo.title}}</div>
+                <div class="meta">{{todo.project}}</div>
+                <div class="extra content">
+                    <span class='right floated edit icon'>
+                        <i class='edit icon'></i>
+                    </span>
+                </div>
+            </div>
+            <div class ='ui bottom attached green basic button' v-show="todo.done">Completed</div>
+            <div class ='ui bottom attached red basic button' v-show="!todo.done">Complete</div>
+        </div>       -->
+        <todo v-on:delete-todo = "deleteTodo" v-for = "todo in todos" :todo = "todo" :key = "todo.project" ></todo>
+    </div>
+</template>
+<script type="text/javascript">
+import Todo from './Todo'
+/* eslint-disable */ 
+export default {
+    props: ['todos'] ,
+    components: {
+        Todo
+    },
+    methods: {
+        deleteTodo(todo) {
+            const todoIndex =  this.todos.indexOf(todo);
+            this.todos.splice(todoIndex,1);
+        },
+    },
+}
+</script>
+<style></style>
